af943b0db4dd0171e6dff1ba1b7d6a2745d3dd49
futokun <futokun@163.com>
2020-02-24 22:15:40 +0800

    重温git

---
+++ src/test/java/com/pwk/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pwk.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("22222222222222222");
+    }
+
+}
